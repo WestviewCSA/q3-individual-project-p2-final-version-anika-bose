93b8fdc87a61fa4a2ab2fcab18f569b543666734
1922063 <1922063@01-18126.pusd.dom>
2026-02-24 10:53:00 -0800

    coordinatate based file reading

--- Q3 Project 1 Period 2/src/ReadMap.java
+++ Q3 Project 1 Period 2/src/ReadMap.java
@@ -11,6 +11,8 @@ public class ReadMap {
 	public ReadMap(String name) {
 		readFile("easyMap1");
 		readFile("medMap2");
+		
+		readCoordinate("easyMap1c");
 
 	}
 
@@ -45,6 +47,50 @@ public class ReadMap {
 					System.out.println();
 				}
 	
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void readCoordinate(String name) {
+		try {
+			File file = new File(name);
+			Scanner scanner = new Scanner(file);
+		/*	for(int i = 0; i<3; i++) {
+				if(!scanner.hasNextInt()) {
+					System.out.println("Invalid Map");
+					return;
+				}
+				scanner.nextInt();
+			}
+			*/
+			int rows = scanner.nextInt();
+			int cols = scanner.nextInt();
+			int rooms = scanner.nextInt(); 
+			System.out.println(rows + " " + cols + " " + rooms);
+			String[][] map = new String[rows*rooms][cols];
+			while(scanner.hasNext()) {
+				String str = scanner.next();
+				int r = scanner.nextInt();
+				int c = scanner.nextInt();
+				int l = scanner.nextInt();
+				map[r+ (r*l)][c] = str;
+				
+			}
+			
+			for(int r = 0; r<map.length; r++) {
+				for(int c = 0; c<map[r].length; c++) {
+					if(map[r][c] == null) {
+						map[r][c] = ".";
+					}
+					System.out.print(map[r][c]);
+				}
+				System.out.println();
+			}
+			
+	 
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
